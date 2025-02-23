83a635b9660da3dd0b893a99d78673ffa008975b
Hafner <peterhafner552@gmail.com>
2025-02-23 13:08:31 +0100

    Implemented navigate in client. Server express to deliver the pages content.

--- client/src/components/TopNavBar/TopNavBar.tsx
+++ client/src/components/TopNavBar/TopNavBar.tsx
@@ -1,9 +1,12 @@
 import { DarkThemeToggle, Navbar, NavbarBrand, NavbarLink, NavbarToggle } from "flowbite-react";
+import { useNavigate } from "react-router-dom";
 
 export default function TopNavBar() {
+  const navigate = useNavigate();
+
   return (
     <Navbar fluid className="w-full  border-b-2 border-b-neutral-300 dark:border-b-white  dark:bg-black dark:bg-opacity-30">
-      <NavbarBrand href="/">
+      <NavbarBrand onClick={() => navigate('/')}>
         <img src="/favicon.png" style={{height: '4vh', width: '4vh', border: 'solid black 1px', borderRadius: '50%'}}></img>
         <span className="pl-5 pr-2 self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           hafnerp.dev
@@ -15,10 +18,10 @@ export default function TopNavBar() {
         </span>
       </NavbarBrand>
       <Navbar.Collapse>
-        <NavbarLink href="/social">
+        <NavbarLink onClick={() => navigate('/social')}>
           Social
         </NavbarLink>
-        <NavbarLink href="/contact">
+        <NavbarLink onClick={() => navigate('/contact')}>
           Contact
         </NavbarLink>
       </Navbar.Collapse>
